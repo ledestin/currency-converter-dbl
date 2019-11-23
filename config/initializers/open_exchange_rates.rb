f46a4aa5e43a4b19556507432be21c820899de9b
Dmitry Maksyoma <ledestin@gmail.com>
2019-11-23 22:19:26 +1300

    Ensure intializer is compatible with zeitwerk

--- config/initializers/open_exchange_rates.rb
+++ config/initializers/open_exchange_rates.rb
@@ -1,4 +1,6 @@
-Money.default_bank = Bank.get(:today) do |new_bank|
-  new_bank.ttl_in_seconds = 86400
-  new_bank.cache = "tmp/money-bank-today-cache.json"
+ActiveSupport.on_load(:bank) do
+  Money.default_bank = Bank.get(:today) do |new_bank|
+    new_bank.ttl_in_seconds = 86400
+    new_bank.cache = "tmp/money-bank-today-cache.json"
+  end
 end
