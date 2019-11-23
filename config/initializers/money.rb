24b3056601a2828f15423acb212a79bcc1f17cf0
Dmitry Maksyoma <ledestin@gmail.com>
2019-11-23 22:22:15 +1300

    Refactor initializers

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,4 +1,9 @@
-require "money"
+ActiveSupport.on_load(:bank) do
+  Money.locale_backend = :i18n
+  I18n.locale = :en
 
-Money.locale_backend = :i18n
-I18n.locale = :en
+  Money.default_bank = Bank.get(:today) do |new_bank|
+    new_bank.ttl_in_seconds = 86400
+    new_bank.cache = "tmp/money-bank-today-cache.json"
+  end
+end
--- config/initializers/open_exchange_rates.rb
+++
@@ -1,6 +0,0 @@
-ActiveSupport.on_load(:bank) do
-  Money.default_bank = Bank.get(:today) do |new_bank|
-    new_bank.ttl_in_seconds = 86400
-    new_bank.cache = "tmp/money-bank-today-cache.json"
-  end
-end
